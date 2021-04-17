f187e453c6be2af947f6606a784d156aff49a4d2
beautifulcode-jpg <ceeellaaardoor@gmail.com>
2021-04-17 16:52:42 +0200

    currency

---
+++ database/migrations/2021_04_17_144253_create_currency_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurrencyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('CURRENCY', function (Blueprint $table) {
+            $table->decimal('CURRENCY_ID',8,0);
+            $table->longText('CURRENCY_TITLE');
+            $table->binary('CURRENCY_PICTURE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currency');
+    }
+}
